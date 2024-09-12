c26c2b08a655c249867660921bbaa4598ac9b58b
toddwh <toddwh@dukes.jmu.edu>
2024-09-12 16:41:45 -0400

    Consolidated the Symbol regex to one regex so its a bit cleaner

--- p1-lexer/src/p1-lexer.c
+++ p1-lexer/src/p1-lexer.c
@@ -16,10 +16,9 @@ TokenQueue* lex (const char* text)
     Regex* whitespace = Regex_new("^[ \t]+");
     Regex* new_line = Regex_new("^\n");
     Regex* letter = Regex_new("^([a-zA-Z][a-zA-Z_0-9]*)");
-    // We need to add [] to the symbol regex for some reason its not catching them
-    Regex* symbol = Regex_new("^([-%*+()[{}}=])");
-    Regex* extra_symbol = Regex_new("^([]\\;])");
-    Regex* double_symbol = Regex_new("^([!><=]=)");
+    // We need to combine the symbols into one regex
+    Regex* symbol = Regex_new("^([-%*+()[{}}=]|[]\\;]|[!><=]=)");
+
     Regex* hex = Regex_new("^(0x[0-9a-fA-F]+)");
     Regex* num = Regex_new("^(0|[1-9][0-9]*)");
     Regex* str_lit = Regex_new("^\"([^\"]*)\"");
@@ -43,10 +42,6 @@ TokenQueue* lex (const char* text)
         } else if (Regex_match(hex, text, match)) {
             TokenQueue_add(tokens, Token_new(HEXLIT, match, line));
             // had to add extra in order to find ]
-        } else if (Regex_match(extra_symbol, text, match)) {
-            TokenQueue_add(tokens, Token_new(SYM, match, line));
-        } else if (Regex_match(double_symbol, text, match)) {
-            TokenQueue_add(tokens, Token_new(SYM, match, line));
         } else if (Regex_match(str_lit, text, match)) {
             TokenQueue_add(tokens, Token_new(STRLIT, match, line));
             // finding a keyword literal
@@ -58,9 +53,6 @@ TokenQueue* lex (const char* text)
             } else {
                 TokenQueue_add(tokens, Token_new(ID, match, line));
             }
-            // Should we check for keywords as well?
-            // I think we may need to instead of having the keyword and illegal before this should be in this if else
-
         } else if (Regex_match(symbol, text, match)) {
             TokenQueue_add(tokens, Token_new(SYM, match, line));
         } else if (Regex_match(num, text, match)){
@@ -89,8 +81,6 @@ TokenQueue* lex (const char* text)
     Regex_free(num);
     Regex_free(hex);
     Regex_free(str_lit);
-    Regex_free(double_symbol);
-    Regex_free(extra_symbol);
     Regex_free(new_line);
     Regex_free(keyword);
     Regex_free(reserved);
