e12fb140a05cf45b79c9c8641b85f916a7b954e7
toddwh <toddwh@dukes.jmu.edu>
2024-09-13 13:26:13 -0400

    Polishing stuff up and adding comments

--- p1-lexer/src/p1-lexer.c
+++ p1-lexer/src/p1-lexer.c
@@ -11,28 +11,27 @@ TokenQueue* lex (const char* text)
         Error_throw_printf("No text");
     }
     TokenQueue* tokens = TokenQueue_new();
- 
+
     /* compile regular expressions */
     Regex* whitespace = Regex_new("^[ \t]+");
     Regex* new_line = Regex_new("^\n");
     Regex* letter = Regex_new("^([a-zA-Z][a-zA-Z_0-9]*)");
-    // We need to combine the symbols into one regex
     Regex* symbol = Regex_new("^([-%*+()[{}}=]|[]\\;]|[!><=]=)");
 
     Regex* hex = Regex_new("^(0x[0-9a-fA-F]+)");
     Regex* num = Regex_new("^(0|[1-9][0-9]*)");
     Regex* str_lit = Regex_new("^\"([^\"]*)\"");
-    // Im not sure why but when the ^ is removed from the keyword regex it starts printing again
     Regex* keyword = Regex_new("^(def|if|else|while|return|break|continue|int|bool|void|true|false)");
     Regex* comment = Regex_new("^//.*");
     // Adding reserved words as illegal
-    Regex* reserved = Regex_new("^(for|callout|class|interface|extends|implements|new|this|string|float|double|null)");
+    Regex* reserved =
+        Regex_new("^(for|callout|class|interface|extends|implements|new|this|string|float|double|null)");
     int line = 1;
     /* read and handle input */
     char match[MAX_TOKEN_LEN];
     char temp[MAX_TOKEN_LEN];
     while (*text != '\0') {
-        
+
         /* match regular expressions */
         if (Regex_match(whitespace, text, match)) {
             // skip whitespace
@@ -40,40 +39,37 @@ TokenQueue* lex (const char* text)
             // New line check
             line++;
         } else if (Regex_match(hex, text, match)) {
+            // finding a hex literal
             TokenQueue_add(tokens, Token_new(HEXLIT, match, line));
-            // had to add extra in order to find ]
         } else if (Regex_match(str_lit, text, match)) {
+            // finding a string literal
             TokenQueue_add(tokens, Token_new(STRLIT, match, line));
-            // finding a keyword literal
-        }else if (Regex_match(letter, text, match)) {
+        } else if (Regex_match(letter, text, match)) {
+            // finding a keyword literal and comparing it to the reserved words
             if (Regex_match(keyword, match, temp) && strcmp(match, temp) == 0) {
                 TokenQueue_add(tokens, Token_new(KEY, temp, line));
-            } else if (Regex_match(reserved, match, temp)  && strcmp(match, temp) == 0){
+                // Error if reserved word is found
+            } else if (Regex_match(reserved, match, temp)  && strcmp(match, temp) == 0) {
                 Error_throw_printf("Resvered Word found at line %d", line, line);
             } else {
                 TokenQueue_add(tokens, Token_new(ID, match, line));
             }
         } else if (Regex_match(symbol, text, match)) {
+            // finding a symbol
             TokenQueue_add(tokens, Token_new(SYM, match, line));
-        } else if (Regex_match(num, text, match)){
+        } else if (Regex_match(num, text, match)) {
+            // finding a decimal literal
             TokenQueue_add(tokens, Token_new(DECLIT, match, line));
         } else if (Regex_match(comment, text, match)) {
             // skip comments
         } else {
-            //printf(text);
-            // This is causing a memory leak for some reason
             Error_throw_printf("Invalid token!");
-            // printf("Invalid token!\n");
-            // printf("%d\n", line);
-            // print hte invalid token
-            // printf("%s\n", tokens->head->text);
-            // printf("%s", text);
         }
- 
+
         /* skip matched text to look for next token */
         text += strlen(match);
     }
- 
+
     /* clean up */
     Regex_free(whitespace);
     Regex_free(letter);
@@ -86,6 +82,5 @@ TokenQueue* lex (const char* text)
     Regex_free(reserved);
     Regex_free(comment);
 
- 
     return tokens;
 }
